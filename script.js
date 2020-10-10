dfb631a104cf128a0d08a2620209c1d08dee24a1
sarahlcox <sarahl.cox.22@gmail.com>
2020-10-10 00:30:40 -0500

    worked on code more still can't get it to work

--- script.js
+++ script.js
@@ -1,63 +1,91 @@
 $(document).ready(function() {
 //get user input address from local storage
-localStorage.getItem("userCity");
+// localStorage.getItem("userCity");
 //get data input and address info on the page
-pushInfo(localStorage.getItem("userCity")); 
+// getInfo(localStorage.getItem("userCity")); 
 var apiKey="09306c19b054cde7fcad1767802d4bbf";
 // api.openweathermap.org/data/2.5/forecast?q=test&appid={API key}
+var dashboard = $(".card-body");
+var userCity;
+var Chicago;
+//     // access the Open Weather API
 
 //     // access the Open Weather API
-    function pushInfo(userCity){
-        //erase/empty page
-        $("#daily-view").empty();
-        var queryURL ="https://api.openweathermap.org/data/2.5/forecast?q="+userCity+"&appid="+apiKey;
+    function getInfo(){
+        //clears dashboard and 5 day containers 
+        // dashboard.empty();
+        // $("#daily-view").empty();
         // console.log(queryURL);
         //fetch from API
+        var currentDayURL ="https://api.openweathermap.org/data/2.5/weather?q="+Chicago+"&appid="+apiKey;
+        var fiveDayURL ="https://api.openweathermap.org/data/2.5/forecast?q="+Chicago+"&appid="+apiKey;
+
         $.ajax({
-            url: queryURL,
+            url: currentDayURL,
             method: "GET",
+
         })
-        // We store all of the retrieved data inside of an object called "fivedayobject"
-        .then(function(fivedayobject) {
-                // for (var i=0;i<5;i++){
+        .then(function (currentWeatherData){
+            console.log(currentDayURL);
+
+            // basically same variables as 5 day
+            // var getDate = new Date (currentWeatherData.list[0].dt_txt)
+            // var dateformat=moment(currentWeatherData.list[i*8].dt_txt).format("ll");
+            // var cityName = $(".cityName").text(currentWeatherData.city.name);
+            // var iconcode = currentWeatherData.list[0].weather[0].icon;
+            // var iconsUrl="http://openweathermap.org/img/wn/"+icons+".png";
+            // var temp=(fiveDayObject.list[i*8].main.temp -273.15) * 1.80 + 32;
+
+
+
+        })
+
+        $.ajax({
+            url: fiveDayURL,
+            method: "GET",
+
+        })
+        // We store all of the retrieved data inside of an object called "fiveDayObject"
+        .then(function(fiveDayObject) {
+                for (i=0;i<5;i++){
                     var newDiv=$("<div>");
-                    newDiv.attr('class','daily-view');
+                    newDiv.attr('class','daily-view col');
+                    $("daily-view").append(newDiv);
                     // newDiv.hide();
-                    var dateList=moment(fivedayobject.list[i*8].dt_txt).format("ll");
-                    var icons=fivedayobject.list[i*8].weather[0].icon;
-                    icons="http://openweathermap.org/img/wn/"+icon+".png";
-                    var temp=(fivedayobject.list[i*8].main.temp -273.15) * 1.80 + 32;
-                    // var humidity=fivedayobject.list[i*8].main.humidity;
-                    //console.log(fivedayobject.list[i*8]);
-                    //  console.log(moment(fivedayobject.list[i*8].dt_txt).format("ll"));
-                    
+                    var dateList=moment(fiveDayObject.list[i*8].dt_txt).format("ll");
+                    var iconcode=fiveDayObject.list[i*8].weather[0].icon;
+                    var iconurl="http://openweathermap.org/img/wn/"+iconcode+".png";
+                    var temp=(fiveDayObject.list[i*8].main.temp -273.15) * 1.80 + 32;
+                    // var humidity=fiveDayObject.list[i*8].main.humidity;
+                    //console.log(fiveDayObject.list[i*8]);
+                    //  console.log(moment(fiveDayObject.list[i*8].dt_txt).format("ll"));
+
                     // Transfer content to HTML
                     //date
                     newDiv.append($("<h3>").html("<strong>"+dateList+"</strong>").attr("class", "date"));
                     //icon
-                    newDiv.append($("<img>").attr("src" + icons).attr("class", "icons"));
-                    // console.log(fivedayobject.list[i*8].weather[0].icon);
+                    newDiv.append($("<img>").attr("src" + iconurl).attr("class", "icons"));
+                    // console.log(fiveDayObject.list[i*8].weather[0].icon);
                     //temp
                     newDiv.append($("<p>").text("Temperature: "+temp.toFixed(2)+"F").attr("class", "temp")); 
-                    // console.log(fivedayobject.list[i*8].main.temp);
+                    // console.log(fiveDayObject.list[i*8].main.temp);
                     //hum
-                    newDiv.append($("<p>").text("Humidity: " + fivedayobject.list[i*8].main.humidity).attr("class", "humidity"));
-                    // console.log(fivedayobject.list[i*8].main.humidity);
-                    $("daily-view").append(newDiv);
+                    newDiv.append($("<p>").text("Humidity: " + fiveDayObject.list[i*8].main.humidity).attr("class", "humidity"));
+                    // console.log(fiveDayObject.list[i*8].main.humidity);
                     
                     // Store user input in localStorage
-                    localStorage.setItem("userInput",response.normalizedInput.city);
-            // }
+                    localStorage.setItem("userInput",response.normalizedInput.userCity);
+            }
         });
     }
 
     // This function handles events where one button is clicked
         // onclick event for search request
-    $("#search-input").on("click", function(event) {
+    $("#search-input").click(function(event) {
         event.preventDefault();
         // This line grabs the input from the textbox
-        var userCity = $("#user-input").val().trim();
-        cityinfo(userCity);
+        userCity = $("#user-input").val().trim();
+        getuserCity();
     });
 
 //create a function to display weather for city
